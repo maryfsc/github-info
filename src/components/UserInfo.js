350d9b8f8ef76087e2142ae3522e2edbbb143ae6
Mary Francis <maryfsc@disroot.org>
2018-11-14 14:11:42 -0200

    Refatoração usando conceito de stateless component do react, transformando classe em função

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 
-class UserInfo extends Component {
-
-  render() {
-    if (this.props.user) {
-      return (
-        <div className="row">
-          <div className="col-lg-4">
-            <img className="img-circle"
-              src={this.props.user.avatar_url}
-              alt="avatar"
-              width="140"
-              height="140" />
-            <h2>{this.props.user.login}</h2>
-            <p>{this.props.user.name}</p>
-            <p>Followers: {this.props.user.followers} / Following: {this.props.user.following}</p>
-            <p><a className="btn btn-default" href={this.props.user.html_url} role="button">View details</a></p>
-          </div>
+function UserInfo(props) {
+  if (props.user) {
+    return (
+      <div className="row">
+        <div className="col-lg-4">
+          <img className="img-circle"
+            src={props.user.avatar_url}
+            alt="avatar"
+            width="140"
+            height="140" />
+          <h2>{props.user.login}</h2>
+          <p>{props.user.name}</p>
+          <p>Followers: {props.user.followers} / Following: {props.user.following}</p>
+          <p><a className="btn btn-default" href={props.user.html_url} role="button">View details</a></p>
         </div>
-      )
-    }
-    return (null);
+      </div>
+    )
   }
+  return (null);
 }
 
 export default UserInfo;
